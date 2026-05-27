8142cbdc2362d12bdec18830dce42a5a04858500
Michael Gorman <michael@gorman.nz>
2026-05-27 11:11:22 +0800

    Commit the small additions for the C file before we make the switch to cpp.
    After more research cpp has access to a more useful
    timing std library. Also going to use cpp vectors for
    ease of use.

--- main.c
+++ main.c
@@ -1,4 +1,22 @@
 #include <stdio.h>
+#include <stdint.h>
+
+typedef struct {
+    float *val;
+    uint64_t n;
+} Vec;
+
+Vec *vecCreate(uint64_t n) {
+
+}
+
+Vec *vecDestroy(uint64_t n) {
+
+}
+
+Vec *dot(Vec *x, Vec *y) {
+
+}
 
 int main(void) {
     printf("let's go\n");
